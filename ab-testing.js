278c83161f0e7b3605cced073ac84b30375d308e
olga.stafaniuk <olga.karolina.stefaniuk@gmail.com>
2017-04-02 20:19:40 +0200

    writing one function which will work for multiple cases - still under development

--- ab-testing.js
+++ ab-testing.js
@@ -1,53 +1,110 @@
 // A library for A/B testing
 
+
+function drawOneCssProperty(whatToTest, optionA, optionB, elementId) {
+  const testedElement = document.getElementById(elementId);
+  var randomNumber = Math.random();
+  if (randomNumber < 0.5) {
+    testedElement.css(whatToTest, optionA);
+  } else {
+    testedElement.css(whatToTest, optionB);
+  }
+}
+
+function drawOneOption(whatToTest, optionA, optionB, elementId) {
+  const testedElement = document.getElementById(elementId);
+  var randomNumber = Math.random();
+  if (randomNumber < 0.5) {
+    testedElement.whatToTest = optionA;
+  } else {
+    testedElement.whatToTest = optionB;
+  }
+}
+
 function testColor(colorA, colorB, elementId) {
-  var randomNumber = Math.random();
-  const testedElement = document.getElementById(elementId);
-  if (randomNumber < 0.5) {
-    testedElement.style.backgroundColor = colorA;
-  } else {
-    testedElement.style.backgroundColor = colorB;
-  }
-}
-
-function testTekst(textA, textB, elementId) {
-  var randomNumber = Math.random();
-  const testedElement = document.getElementById(elementId);
-  if (randomNumber < 0.5) {
-    testedElement.innerHTML = textA;
-  } else {
-    testedElement.innerHTML = textB;
-  }
-}
-
-function testImage(imageSrcA, imageSrcB, elementId) {
-  var randomNumber = Math.random();
-  const testedElement = document.getElementById(elementId);
-  if (randomNumber < 0.5) {
-    testedElement.src = imageSrcA;
-  } else {
-    testedElement.src = imageSrcB;
-  }
-}
-
-function testDisplay(elementId) {
-  var randomNumber = Math.random();
-  const testedElement = document.getElementById(elementId);
-  if (randomNumber < 0.5) {
-    testedElement.style.display = 'none';
-  } else {
-    testedElement.style.display = 'inline-block';
-  }
+    drawOneCssProperty('backgroundColor', colorA, colorB, elementId);
 }
 
 function testTextColor(colorA, colorB, elementId) {
-  var randomNumber = Math.random();
-  const testedElement = document.getElementById(elementId);
-  if (randomNumber < 0.5) {
-    testedElement.style.color = colorA;
-  } else {
-    testedElement.style.color = colorB;
-  }
+    drawOneCssProperty('color', colorA, colorB, elementId);
 }
+
+function testDisplay(elementId) {
+    drawOneCssProperty('display', 'none', 'inline-block', elementId);
+}
+
+function testTekst(textA, textB, elementId) {
+    drawOneOption('innerHTML', textA, textB, elementId);
+}
+
+function testImage(imageSrcA, imageSrcB, elementId) {
+    drawOneOption('src', imageSrcA, imageSrcB, elementId);
+}
+
+
+// function drawOneOption(whatToTest, optionA, optionB, elementId) {
+//     const testedElement = document.getElementById(elementId);
+//     var randomNumber = Math.random();
+//     if (randomNumber < 0.5) {
+//         var testedProperties;
+//         for (i = 0, i < whatToTest.length, i += 1) {
+//             testedProperties += whatToTest[i];
+//         };
+//         testedElement.testedProperties = optionA;
+//     } else {
+//         testedElement.testedProperties = optionB;
+//     }
+// }
+
+
+// function testColor(colorA, colorB, elementId) {
+//   var randomNumber = Math.random();
+//   const testedElement = document.getElementById(elementId);
+//   if (randomNumber < 0.5) {
+//     testedElement.style.backgroundColor = colorA;
+//   } else {
+//     testedElement.style.backgroundColor = colorB;
+//   }
+// }
+
+// function testTekst(textA, textB, elementId) {
+//   var randomNumber = Math.random();
+//   const testedElement = document.getElementById(elementId);
+//   if (randomNumber < 0.5) {
+//     testedElement['innerHTML'] = textA;
+//   } else {
+//     testedElement['innerHTML'] = textB;
+//   }
+// }
+
+// function testImage(imageSrcA, imageSrcB, elementId) {
+//   var randomNumber = Math.random();
+//   const testedElement = document.getElementById(elementId);
+//   if (randomNumber < 0.5) {
+//     testedElement.src = imageSrcA;
+//   } else {
+//     testedElement.src = imageSrcB;
+//   }
+// }
+
+// function testDisplay(elementId) {
+//   var randomNumber = Math.random();
+//   const testedElement = document.getElementById(elementId);
+//   if (randomNumber < 0.5) {
+//     testedElement.style.display = 'none';
+//   } else {
+//     testedElement.style.display = 'inline-block';
+//   }
+// }
+//
+// function testTextColor(colorA, colorB, elementId) {
+//   var randomNumber = Math.random();
+//   const testedElement = document.getElementById(elementId);
+//   if (randomNumber < 0.5) {
+//     testedElement.style.color = colorA;
+//   } else {
+//     testedElement.style.color = colorB;
+//   }
+// }
 
 export { testColor,  testTekst, testImage, testDisplay, testTextColor};
